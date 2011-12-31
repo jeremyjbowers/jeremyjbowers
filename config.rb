b1b3766497db3cbe3f9f7434f9a10cfed8d07ccc
Jeremy Bowers <jeremyjbowers@gmail.com>
2011-12-30 21:02:21 -0500

    your message

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/jeremyjbowers/"
+http_images_path = "/jeremyjbowers/images"
+http_fonts_path = "/jeremyjbowers/fonts"
+css_dir = "public/jeremyjbowers/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
